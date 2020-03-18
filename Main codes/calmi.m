79caf2c70db1ea63e37294696ab10e34bfbc2115
RPcb <61924555+RPcb@users.noreply.github.com>
2020-03-18 22:45:53 +0800

    Function of select correlated variables

---
+++ Main codes/calmi.m
@@ -0,0 +1,23 @@
+function mi = calmi(u1, u2, win_size)
+x = [u1, u2];
+n = win_size;
+[xrow, xcol] = size(x);
+bin = zeros(xrow,xcol);
+pmf = zeros(n, 2);
+for i = 1:2
+    minx = min(x(:,i));
+    maxx = max(x(:,i));
+    binwidth = (maxx - minx) / n;
+    edges = minx + binwidth*(0:n);
+    histcEdges = [-Inf edges(2:end-1) Inf];
+    [occur,bin(:,i)] = histc(x(:,i),histcEdges,1); 
+    pmf(:,i) = occur(1:n)./xrow;
+end
+ 
+jointOccur = accumarray(bin,1,[n,n]);  
+jointPmf = jointOccur./xrow;
+Hx = -(pmf(:,1))'*log2(pmf(:,1)+eps);
+Hy = -(pmf(:,2))'*log2(pmf(:,2)+eps);
+Hxy = -(jointPmf(:))'*log2(jointPmf(:)+eps);
+MI = Hx+Hy-Hxy;
+mi = MI/sqrt(Hx*Hy);
