415db42bf089dee149b15d6b629a9d6a27852372
RPcb <61924555+RPcb@users.noreply.github.com>
2020-04-29 10:48:05 +0800

    Update Main_ARNN.m

--- Main codes/Main_ARNN.m
+++ Main codes/Main_ARNN.m
@@ -122,6 +122,7 @@ while ii<3000                    % run each case sequentially with different ini
     
     myreal=real_y(trainlength+1:trainlength+predict_len-1);
     RMSE = sqrt(immse(union_predict_y_ARNN, myreal));
+    RMSE = RMSE /(std(myreal) + 0.001);      % normalized RMSE
     if RMSE < 0.5
         Accurate_predictions = Accurate_predictions + 1;
     end
