ba38d378f68d2c06d77c9b57df39844720212619
RPcb <61924555+RPcb@users.noreply.github.com>
2020-07-18 16:40:08 +0800

    mylorenz.m

--- Main codes/mylorenz.m
+++ Main codes/mylorenz.m
@@ -5,13 +5,14 @@ stepsize=0.02;
 l=round(time/stepsize);
 %N=12;
 x=zeros(3*N,l);
+
 x(:,1)=-0.1:0.003:(-0.1+0.003*(3*N-1));
 % Lorenz system
 C=0.1;
 
 for i=1:l-1
     
-    x(1,i+1)=x(1,i)+stepsize*(10*(x(2,i)-x(1,i))+C*x(1,i));
+    x(1,i+1)=x(1,i)+stepsize*(10*(x(2,i)-x(1,i))+C*x(1+(N-1)*3,i));
     x(2,i+1)=x(2,i)+stepsize*(28*x(1,i)-x(2,i)-x(1,i)*x(3,i));
     x(3,i+1)=x(3,i)+stepsize*(-8/3*x(3,i)+x(1,i)*x(2,i));
     for j=1:N-1
